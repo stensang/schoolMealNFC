9e0f1405a318d42ff48e66ffa56ad2b8243a3b4f
Sten Sang <stensang@hotmail.com>
2018-01-10 23:36:51 +0200

    New templates and routes for Flask App

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,11 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return 'INDEX3'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
