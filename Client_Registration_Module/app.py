c6cf830e7b0ee6f936fd178dcd1a09beed255a8d
Sten Sang <sten.sang@hektor1.ttu.ee>
2018-02-03 22:09:08 +0200

    USE CASE UC1: söögikorra registreerimine

--- Client_Registration_Module/app.py
+++ Client_Registration_Module/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -15,7 +16,15 @@ class widgetClock(Label):
 
 
 class TestApp(App):
+
     def build(self):
+
+        mealsToRegisterRequest = requests.get('http://127.0.0.1:8080/meals-to-register')
+        if mealsToRegisterRequest.status_code != 200:
+            # This means something went wrong.
+            raise ApiError('GET /tasks/ {}'.format(mealsToRegisterRequest.status_code))
+
+        mealsToRegister = mealsToRegisterRequest.json()
 
         layout = BoxLayout(orientation = 'horizontal')
         layoutDate = BoxLayout(orientation='vertical')
@@ -25,13 +34,16 @@ class TestApp(App):
         Clock.schedule_interval(widgetClock1.update, 1)
         layoutDate.add_widget(widgetClock1)
 
-        buttonLunch = Button(text="Lõunasöök")
-        buttonAdditional = Button(text="Lisaeine")
-        buttonBreakfast = Button(text="Hommikusöök")
+        for meal in mealsToRegister["mealsToRegister"]:
+            layoutButtons.add_widget(Button(text=meal['name']))
 
-        layoutButtons.add_widget(buttonLunch)
-        layoutButtons.add_widget(buttonAdditional)
-        layoutButtons.add_widget(buttonBreakfast)
+        # buttonLunch = Button(text="Lõunasöök")
+        # buttonAdditional = Button(text="Lisaeine")
+        # buttonBreakfast = Button(text="Hommikusöök")
+        #
+        # layoutButtons.add_widget(buttonLunch)
+        # layoutButtons.add_widget(buttonAdditional)
+        # layoutButtons.add_widget(buttonBreakfast)
 
         layout.add_widget(layoutDate)
         layout.add_widget(layoutButtons)
