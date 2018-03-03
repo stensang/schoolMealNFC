34b06f23d973396523892825e02f8c50d1cfaf2f
Sten Sang <stensang@hotmail.com>
2018-03-03 17:29:51 +0200

    USE CASE UC1: söögikorra registreerimine

--- Client_Registration_Module/app.py
+++ Client_Registration_Module/app.py
@@ -3,11 +3,12 @@
 
 import requests
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock, mainthread
-import threading
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 import time
 import signal
 import MFRC522
@@ -16,7 +17,7 @@ class widgetClock(Label):
     def update(self, *args):
         self.text = time.asctime()
 
-class mealsToRegister():
+class AvailableMeals():
     # variables
     getUrl = 'http://192.168.1.186:5000/soogikorrad-registreerimisele-avatud'
     postUrl = 'http://192.168.1.186:5000/opilase-soogikorrad'
@@ -27,29 +28,24 @@ class mealsToRegister():
         self.getResponse = requests.get(self.getUrl)
         if self.getResponse.status_code != 200:
             # This means something went wrong.
-            raise ApiError('GET /tasks/ {}'.format(self.mealsToRegisterRequest.status_code))
+            raise ApiError('GET /tasks/ {}'.format(self.AvailableMealsRequest.status_code))
         return self.getResponse.json()
     # Saadab andmed serverile
-    def post(self, payload):
+    def register(self, payload):
         self.postResponse = requests.post(self.postUrl, json = payload)
         return self.postResponse
 
-class nfcReader(Label):
+class CardReader():
 
-    mr = mealsToRegister()
+    #am = AvailableMeals()
 
-    def update(self, *args):
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
 
-        self.text = "Näita kaarti!"
-        # Create an object of the class MFRC522
-        self.MIFAREReader = MFRC522.MFRC522()
+    def getUID(self, *args):
 
         # Scan for cards
         (self.status,self.TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
-
-        # If a card is found
-        if self.status == self.MIFAREReader.MI_OK:
-            print("Card detected")
 
         # Get the UID of the card
         (self.status, uid) = self.MIFAREReader.MFRC522_Anticoll()
@@ -58,43 +54,76 @@ class nfcReader(Label):
         if self.status == self.MIFAREReader.MI_OK:
 
             uidSTR = ''.join(map(str, uid))
-            self.text = uidSTR
 
             # Testin API-t
-            self.data = {'uid': uidSTR, 'soogikorrad': [{'soogikorra_id': 1}, {'soogikorra_id': 2}, {'soogikorra_id': 3}]}
-            self.mr.post(self.data)
+            # self.data = {'uid': uidSTR, 'soogikorrad': [{'soogikorra_id': 1}, {'soogikorra_id': 2}, {'soogikorra_id': 3}]}
+            # self.am.register(self.data)
 
-class TestApp(App):
+            return uidSTR
+
+class CardUID(Label):
+
+    nfc = CardReader()
+
+    def update(self, *args):
+        if RaspberryPi.sm.current != 'main':
+            time.sleep(0.5)
+            RaspberryPi.sm.current = 'main'
+        self.text = "Näita kaarti!"
+        self.uidSTR = self.nfc.getUID()
+        if self.uidSTR is not None:
+            RaspberryPi.sm.current = 'response'
+            #self.text = self.uidSTR
+
+class Meals(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(Meals, self).__init__(**kwargs)
+
+        am = AvailableMeals()
+        meals = am.get()
+
+        for meal in meals:
+            self.add_widget(ToggleButton(text=meal['nimetus'], state= 'down' if meal['vaikimisi'] else 'normal'))
+
+class Registrator(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(Registrator, self).__init__(**kwargs)
+
+        self.layoutDate = BoxLayout(orientation='vertical')
+        self.layoutMeals = Meals(orientation='vertical')
+
+        self.widgetClock1 = widgetClock()
+        Clock.schedule_interval(self.widgetClock1.update, 1)
+        self.layoutDate.add_widget(self.widgetClock1)
+
+        self.cardUID = CardUID()
+
+        Clock.schedule_interval(self.cardUID.update, 0.5)
+        self.layoutDate.add_widget(self.cardUID)
+
+        self.add_widget(self.layoutDate)
+        self.add_widget(self.layoutMeals)
+
+
+class RaspberryPi(App):
+
+    sm = ScreenManager(transition=NoTransition())
 
     def build(self):
 
-        layout = BoxLayout(orientation = 'horizontal')
-        layoutDate = BoxLayout(orientation='vertical')
-        layoutButtons = BoxLayout(orientation='vertical')
+        self.ms = Screen(name='main')
+        self.rt = Registrator()
+        self.ms.add_widget(self.rt)
 
-        widgetClock1 = widgetClock()
-        Clock.schedule_interval(widgetClock1.update, 1)
-        layoutDate.add_widget(widgetClock1)
+        self.rs = Screen(name='response')
+        self.rs.add_widget(Label(text='Hello world!'))
 
-        mealsToRegister1 = mealsToRegister()
-        meals = mealsToRegister1.get()
+        self.sm.add_widget(self.ms)
+        self.sm.add_widget(self.rs)
 
-        for meal in meals:
-            layoutButtons.add_widget(ToggleButton(text=meal['nimetus'], state= 'down' if meal['vaikimisi'] else 'normal'))
-
-        nfc = nfcReader()
-
-        Clock.schedule_interval(nfc.update, 0.5)
-        layoutDate.add_widget(nfc)
-
-        # rs = registrationStatus()
-        # Clock.schedule_interval(rs.update, 1)
-        # layoutDate.add_widget(rs)
-
-        layout.add_widget(layoutDate)
-        layout.add_widget(layoutButtons)
-
-        return layout
+        return self.sm
 
 if __name__ == '__main__':
-    TestApp().run()
+    RaspberryPi().run()
